ebc1d08853da5a159e076ee3386334af4b448967
Sina Kahrobaei <sina@pop-os.localdomain>
2022-03-01 22:05:35 -0600

    front components, reducers, and actions added

---
+++ client/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const FLIP_CARD = "FLIP_CARD";
+export const NEXT_CARD = "NEXT_CARD";
+export const PREVIOUS_CARD = "PREVIOUS_CARD";
+export const UPDATE_RATING = "UPDATE_RATING";
+export const GET_ALL_CARDS = "GET_ALL_CARDS";
